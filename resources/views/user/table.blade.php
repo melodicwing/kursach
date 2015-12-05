1622f48ee5b4ab63fdab70f88c0e57f4fb33afd7
Dima <melodicwing@yahoo.com>
2015-12-05 16:57:03 +0300

    table page small enhancements with time selects

--- resources/views/user/table.blade.php
+++ resources/views/user/table.blade.php
@@ -15,50 +15,58 @@
 					<label>{{ trans('user/table.from') }}</label>
 					<div class='row'>
 						<div class='col-xs-6'>
-							<label for='input__from_h'>{{ trans('user/table.from_h') }}</label>
-							<select class='form-control' id='input__from_h'>
-								<option>00</option>
-								<option>01</option>
-								<option>02</option>
-								<option>03</option>
-								<option>04</option>
-								<option>05</option>
-								<option>06</option>
-								<option>07</option>
-								<option>08</option>
-								<option>09</option>
-								<option>10</option>
-								<option>11</option>
-								<option>12</option>
-								<option>13</option>
-								<option>14</option>
-								<option>15</option>
-								<option>16</option>
-								<option>17</option>
-								<option>18</option>
-								<option>19</option>
-								<option>20</option>
-								<option>21</option>
-								<option>22</option>
-								<option>23</option>
-							</select>
+							<div class='row'>
+								<label for='input__from_h' class='col-xs-6 label__padding-top_7'>{{ trans('user/table.from_h') }}</label>
+								<div class='col-xs-6'>
+									<select class='form-control' id='input__from_h'>
+										<option>00</option>
+										<option>01</option>
+										<option>02</option>
+										<option>03</option>
+										<option>04</option>
+										<option>05</option>
+										<option>06</option>
+										<option>07</option>
+										<option>08</option>
+										<option>09</option>
+										<option>10</option>
+										<option>11</option>
+										<option>12</option>
+										<option>13</option>
+										<option>14</option>
+										<option>15</option>
+										<option>16</option>
+										<option>17</option>
+										<option>18</option>
+										<option>19</option>
+										<option>20</option>
+										<option>21</option>
+										<option>22</option>
+										<option>23</option>
+									</select>
+								</div>
+							</div>
 						</div>
 						<div class='col-xs-6'>
-							<label for='input__from_m'>{{ trans('user/table.from_m') }}</label>
-							<select class='form-control' id='input__from_m'>
-								<option>00</option>
-								<option>05</option>
-								<option>10</option>
-								<option>15</option>
-								<option>20</option>
-								<option>25</option>
-								<option>30</option>
-								<option>35</option>
-								<option>40</option>
-								<option>45</option>
-								<option>50</option>
-								<option>55</option>
-							</select>
+							<div class='row'>
+								<label for='input__from_m' class='col-xs-6 label__padding-top_7'>{{ trans('user/table.from_m') }}</label>
+								<div class='col-xs-6'>
+									<select class='form-control' id='input__from_m'>
+										<option>00</option>
+										<option>05</option>
+										<option>10</option>
+										<option>15</option>
+										<option>20</option>
+										<option>25</option>
+										<option>30</option>
+										<option>35</option>
+										<option>40</option>
+										<option>45</option>
+										<option>50</option>
+										<option>55</option>
+									</select>
+								</div>
+							</div>
 						</div>
 					</div>
 				</div>
@@ -67,50 +75,58 @@
 					<label>{{ trans('user/table.to') }}</label>
 					<div class='row'>
 						<div class='col-xs-6'>
-							<label for='input__to_h'>{{ trans('user/table.to_h') }}</label>
-							<select class='form-control' id='input__to_h'>
-								<option>00</option>
-								<option>01</option>
-								<option>02</option>
-								<option>03</option>
-								<option>04</option>
-								<option>05</option>
-								<option>06</option>
-								<option>07</option>
-								<option>08</option>
-								<option>09</option>
-								<option>10</option>
-								<option>11</option>
-								<option>12</option>
-								<option>13</option>
-								<option>14</option>
-								<option>15</option>
-								<option>16</option>
-								<option>17</option>
-								<option>18</option>
-								<option>19</option>
-								<option>20</option>
-								<option>21</option>
-								<option>22</option>
-								<option>23</option>
-							</select>
+							<div class='row'>
+								<label for='input__to_h' class='col-xs-6 label__padding-top_7'>{{ trans('user/table.to_h') }}</label>
+								<div class='col-xs-6'>
+									<select class='form-control' id='input__to_h'>
+										<option>00</option>
+										<option>01</option>
+										<option>02</option>
+										<option>03</option>
+										<option>04</option>
+										<option>05</option>
+										<option>06</option>
+										<option>07</option>
+										<option>08</option>
+										<option>09</option>
+										<option>10</option>
+										<option>11</option>
+										<option>12</option>
+										<option>13</option>
+										<option>14</option>
+										<option>15</option>
+										<option>16</option>
+										<option>17</option>
+										<option>18</option>
+										<option>19</option>
+										<option>20</option>
+										<option>21</option>
+										<option>22</option>
+										<option>23</option>
+									</select>
+								</div>
+							</div>
 						</div>
 						<div class='col-xs-6'>
-							<label for='input__to_m'>{{ trans('user/table.to_m') }}</label>
-							<select class='form-control' id='input__to_m'>
-								<option>00</option>
-								<option>05</option>
-								<option>10</option>
-								<option>15</option>
-								<option>20</option>
-								<option>25</option>
-								<option>30</option>
-								<option>35</option>
-								<option>40</option>
-								<option>45</option>
-								<option>50</option>
-								<option>55</option>
-							</select>
+							<div class='row'>
+								<label for='input__to_m' class='col-xs-6 label__padding-top_7'>{{ trans('user/table.to_m') }}</label>
+								<div class='col-xs-6'>
+									<select class='form-control' id='input__to_m'>
+										<option>00</option>
+										<option>05</option>
+										<option>10</option>
+										<option>15</option>
+										<option>20</option>
+										<option>25</option>
+										<option>30</option>
+										<option>35</option>
+										<option>40</option>
+										<option>45</option>
+										<option>50</option>
+										<option>55</option>
+									</select>
+								</div>
+							</div>
 						</div>
 					</div>
 				</div>
